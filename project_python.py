dc333014e4eca9468e2335fafa55abe28a19f211
alexeyfnoise <alexeyfnoise@yandex.ru>
2025-02-24 21:56:11 +0500

    Add

--- project_python.py
+++ project_python.py
@@ -94,3 +94,21 @@ if (x1 - y1 == x2 - y2) or (x1 + y1 == x2 + y2):
     print('YES')
 else:
     print('NO')
+
+#Ход коня
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+if (x1 - x2) ** 2 + (y1 - y2) ** 2 == 5:
+    print("YES")
+else:
+    print("NO")
+
+#Ход ферзя
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) == abs(y1 - y2) or x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
+

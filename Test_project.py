e58de1912689e6961a351859547a2e8d4a166d94
Alexey <alexeyfnoise@yandex.ru>
2025-03-24 20:29:03 +0500

    11.3 Методы списков. Часть 1

--- Test_project.py
+++ Test_project.py
@@ -1,1 +1,7 @@
-# Файл для отладки задач по курсам
+numbers1 = [1, 2, 3]
+numbers2 = [6]
+numbers3 = [7, 8, 9, 10, 11, 12, 13]
+numbers1 *= 2
+numbers2 *= 9
+numbers1 += numbers2 + numbers3
+print(numbers1)
